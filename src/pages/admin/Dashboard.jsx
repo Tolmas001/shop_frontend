8b04596ba9b97bad490a80b671439faaff76a91b
Tolmas001 <urinovtolmas20@gmail.com>
2026-07-04 16:25:27 +0500

    ewlkdsnwes

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -60,7 +60,7 @@ import {
   Store,
   Search,
   RotateCcw,
-  BarChart3
+  BarChart
 } from 'lucide-react';
 
 import {
@@ -184,7 +184,7 @@ const AdminDashboard = () => {
             <RotateCcw size={19} /> Qaytarishlar
           </Link>
           <Link to="/admin/coupons-analytics" className={`sidebar-item ${isActive('/admin/coupons-analytics') ? 'active' : ''}`}>
-            <BarChart3 size={19} /> Promo statistika
+            <BarChart size={19} /> Promo statistika
           </Link>
           <Link to="/admin/wishlist-analytics" className={`sidebar-item ${isActive('/admin/wishlist-analytics') ? 'active' : ''}`}>
             <Heart size={19} /> Wishlist statistika
